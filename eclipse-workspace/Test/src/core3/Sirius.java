294702c758234501efd049b760f6385d3f408fdc
Anton <antonyourkevich09@tut.by>
2019-02-25 16:54:06 +0300

    Secondy commit

---
+++ eclipse-workspace/Test/src/core3/Sirius.java
@@ -0,0 +1,26 @@
+package core3;
+
+public class Sirius extends Star{
+	
+	public static int population4 = 25000;
+	public static String polution4 = "very poluted";
+	
+	public static int method() 
+	{
+		return population4;
+	}
+	
+	
+	public static String method2() 
+	{
+		return polution4;
+	}
+
+	
+	
+
+	
+	
+	
+
+}
